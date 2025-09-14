ccee255f8394cd8b630608ad6b96f532dde18b32
himanshu chauhan <himanshun00002@gmail.com>
2025-09-14 16:19:18 +0530

    Move zoom_connection to root for Render deployment

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,16 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import zoom_connection.routing
+import zoom_connection.routing  # This works if folder is in the right place
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            zoom_connection.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
